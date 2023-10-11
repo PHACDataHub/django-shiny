6feea9ae3a4de7d8df90b0f9a852beff28c50eda
Jules Kuehn <jk@jules.lol>
2023-10-11 09:12:12 -0400

    apply k8s from docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+gcloud secrets versions access 1 --secret=gcp_service_account_key > ./djangoapp/gcp_service_account_key.json
+gcloud container clusters get-credentials django-shiny --region=northamerica-northeast1
 
 python ./djangoapp/manage.py makemigrations
 python ./djangoapp/manage.py migrate
 python ./djangoapp/manage.py runscript create_user_groups
-gcloud secrets versions access 1 --secret=gcp_service_account_key > ./djangoapp/gcp_service_account_key.json
 
 # Exec the CMD from the Dockerfile
 exec "$@"
