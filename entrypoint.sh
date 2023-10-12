8b87fd5c04bd5bfbac0534affbdd395f29a5f28b
Jules Kuehn <jk@jules.lol>
2023-10-12 16:12:35 -0400

    sort apps; manually set gcp creds in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,11 @@
 python ./djangoapp/manage.py makemigrations
 python ./djangoapp/manage.py migrate
 python ./djangoapp/manage.py runscript create_user_groups
+# python ./djangoapp/manage.py collectstatic --noinput
 
-# Wait until IAM is available through GKE Worload Identity
-sleep 90
+gcloud config set account django-shiny-devops@phx-datadissemination.iam.gserviceaccount.com --quiet
+gcloud config set project phx-datadissemination --quiet
+gcloud config set run/region northamerica-northeast1 --quiet
 # Get the service account key from Secret Manager (necessary for Cloud Storage URL signing)
 gcloud secrets versions access 1 --secret=gcp_service_account_key > ./djangoapp/gcp_service_account_key.json
 # Get Kubernetes credentials
