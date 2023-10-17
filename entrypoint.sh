89f8abbcd6e313ccbdaece3dec6707f5b571f398
Jules Kuehn <jk@jules.lol>
2023-10-17 17:41:25 -0400

    service account key in secrets.yaml

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,11 @@
 # Prepare Django environment
 python ./djangoapp/manage.py makemigrations
 python ./djangoapp/manage.py migrate
-python ./djangoapp/manage.py runscript create_user_groups
+python ./djangoapp/manage.py runscript setup
 # python ./djangoapp/manage.py collectstatic --noinput
 
-gcloud config set account django-shiny-devops@phx-datadissemination.iam.gserviceaccount.com --quiet
-gcloud config set project phx-datadissemination --quiet
-gcloud config set run/region northamerica-northeast1 --quiet
-# Get the service account key from Secret Manager (necessary for Cloud Storage URL signing)
-gcloud secrets versions access 1 --secret=gcp_service_account_key > ./djangoapp/gcp_service_account_key.json
-# Get Kubernetes credentials
+# Service account key is at ./gcp_service_account_key.json
+gcloud auth activate-service-account --key-file=./gcp_service_account_key.json
 gcloud container clusters get-credentials django-shiny --region=northamerica-northeast1
 
 # Exec the CMD from the Dockerfile (gunicorn)
