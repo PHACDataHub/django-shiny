8cc7ec7dcfc1b3511ad863a7a372104685602857
Andrew Guo <andrew.c.guo@gmail.com>
2024-03-05 12:20:50 -0500

    update cloudbuild trigger template and header title

--- run_cloudbuild.sh
+++ run_cloudbuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcloud builds triggers run django-shiny-repo-trigger --region=northamerica-northeast1 --branch=prod
+gcloud builds triggers run django-shiny-repo-trigger --region=northamerica-northeast1 --branch=prod --project=pht-01hhmqtnrpf
