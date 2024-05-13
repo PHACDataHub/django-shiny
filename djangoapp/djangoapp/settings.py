3e52006fb361532a6e6e5d9e0d10d239db7328a4
Andrew Guo <andrew.c.guo@gmail.com>
2024-05-13 15:39:16 -0400

    update allowed domains to phac-aspc.gc.ca

--- djangoapp/djangoapp/settings.py
+++ djangoapp/djangoapp/settings.py
@@ -86,19 +86,19 @@ AUTHENTICATION_BACKENDS = (
 )
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://shiny.alpha.phac.gc.ca​​',
-    'https://test.shiny.alpha.phac.gc.ca​​',
-    'https://dev.shiny.alpha.phac.gc.ca​​',
+    'https://shiny.alpha.phac-aspc.gc.ca​​',
+    'https://test.shiny.alpha.phac-aspc.gc.ca​​',
+    'https://dev.shiny.alpha.phac-aspc.gc.ca​​',
 ]
 CSRF_ALLOWED_ORIGINS = [
-    'https://shiny.alpha.phac.gc.ca​​',
-    'https://test.shiny.alpha.phac.gc.ca​​',
-    'https://dev.shiny.alpha.phac.gc.ca​​',
+    'https://shiny.alpha.phac-aspc.gc.ca​​',
+    'https://test.shiny.alpha.phac-aspc.gc.ca​​',
+    'https://dev.shiny.alpha.phac-aspc.gc.ca​​',
 ]
 CORS_ORIGINS_WHITELIST = [
-    'https://shiny.alpha.phac.gc.ca​​',
-    'https://test.shiny.alpha.phac.gc.ca​​',
-    'https://dev.shiny.alpha.phac.gc.ca​​',
+    'https://shiny.alpha.phac-aspc.gc.ca​​',
+    'https://test.shiny.alpha.phac-aspc.gc.ca​​',
+    'https://dev.shiny.alpha.phac-aspc.gc.ca​​',
 ]
 
 LOGIN_URL = "email_login"
