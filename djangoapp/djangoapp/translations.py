3e261a7ceab83d19b75a95c20a96d7679ccb7c7e
Andrew Guo <andrew.c.guo@gmail.com>
2024-06-04 11:28:30 -0400

    translated homepage

---
+++ djangoapp/djangoapp/translations.py
@@ -0,0 +1,101 @@
+translation_entries = {
+    "site_title": {
+        "en": "PHAC Shiny Dev",
+        "fr": "ASPC Shiny Dev",
+    },
+    "site_subtitle": {
+        "en": "Data Collection and Retrieval Application",
+        "fr": "Collecte et de récupération des données",
+    },
+    "other_lang": {
+        "en": "Français",
+        "fr": "English",
+    },
+    "log_in": {
+        "en": "Log in",
+        "fr": "Se connecter",
+    },
+    "logout": {"en": "Logout", "fr": "Fermeture de session"},
+    "welcome": {
+        "en": "Welcome",
+        "fr": "Bienvenue",
+    },
+    "save": {
+        "en": "Save",
+        "fr": None,
+        "translated_using": "Save",
+        "fr_auto": "Enregistrer",
+    }, 
+    "app_directory": {
+        "en": "App Directory",
+        "fr": None,
+        "translated_using": "App Directory",
+        "fr_auto": "Répertoire des applications",
+    },
+    "manage_apps": {
+        "en": "Manage Apps",
+        "fr": None,
+        "translated_using": "Manage Apps",
+        "fr_auto": "Gérer les applications",
+    },
+    "manage_users": {
+        "en": "Manage Users",
+        "fr": None,
+        "translated_using": "Manage Users",
+        "fr_auto": "Gérer les utilisateurs",
+    },
+    "about_app_directory_title": {
+        "en": "About the PHAC Shiny App Directory",
+        "fr": None,
+        "translated_using": "About the PHAC Shiny App Directory",
+        "fr_auto": "À propos du répertoire des applications Shiny de l'ASPC",
+    },
+    "about_app_directory_text1": {
+        "en": "This is an experimental space within the Public Health Agency of Canada (PHAC). It hosts interactive data apps built with R Shiny, and provides access controls.",
+        "fr": None,
+        "translated_using": "This is an experimental space within the Public Health Agency of Canada (PHAC). It hosts interactive data apps built with R Shiny, and provides access controls.",
+        "fr_auto": "Ceci est un espace expérimental au sein de l'Agence de la santé publique du Canada (ASPC). Il héberge des applications de données interactives construites avec R Shiny et fournit des contrôles d'accès.",
+    },
+    "about_app_directory_text2": {
+        "en": "Some apps are publicly accessible, while others are limited to certain users.",
+        "fr": None,
+        "translated_using": "Some apps are publicly accessible, while others are limited to certain users.",
+        "fr_auto": "Certaines applications sont accessibles au public, tandis que d'autres sont limitées à certains utilisateurs.",
+    },
+    "only_show_accessible_apps": {
+        "en": "Only show apps that I can access",
+        "fr": None,
+        "translated_using": "Only show apps that I can access",
+        "fr_auto": "Afficher uniquement les applications auxquelles j'ai accès",
+    },
+    "anonymous_user_subtitle": {
+        "en": "you might see more apps if you",
+        "fr": None,
+        "translated_using": "you might see more apps if you",
+        "fr_auto": "vous pourriez voir plus d'applications si vous",
+    },
+    "technical_info": {
+        "en": "For technical information, visit",
+        "fr": None,
+        "translated_using": "For technical information, visit",
+        "fr_auto": "Pour obtenir des informations techniques, visitez",
+    },
+    "on_github": {
+        "en": "on GitHub or",
+        "fr": None,
+        "translated_using": "on GitHub or",
+        "fr_auto": "sur GitHub ou",
+    },
+    "email_ds": {
+        "en": "email the Data Science team",
+        "fr": None,
+        "translated_using": "email us",
+        "fr_auto": "envoyez-nous un email",
+    },
+    "at_phac": {
+        "en": "at Public Health Agency of Canada (PHAC)",
+        "fr": None,
+        "translated_using": "at Public Health Agency of Canada (PHAC)",
+        "fr_auto": "à l'Agence de la santé publique du Canada (ASPC)",
+    },
+}
